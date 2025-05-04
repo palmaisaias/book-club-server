07be0e6d1ef032176a453f91d556c7ba37de43f6
palmaisaias <palmaisaias@yahoo.com>
2025-05-04 16:48:22 -0700

    Ignore changes

--- app/core/config.py
+++ app/core/config.py
@@ -1,5 +1,9 @@
 import os
+from dotenv import load_dotenv
 
-SECRET_KEY = os.getenv("SECRET_KEY", "supersecretkey")  
+# load server/.env
+load_dotenv(os.path.join(os.path.dirname(__file__), os.pardir, ".env"))
+
+SECRET_KEY = os.getenv("SECRET_KEY", "supersecretkey")
 ALGORITHM = "HS256"
 ACCESS_TOKEN_EXPIRE_MINUTES = 30
